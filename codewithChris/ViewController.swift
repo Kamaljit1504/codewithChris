cc4b5ac9b911bd9a21e741cfa7c36edb4c0f76f5
Kamaljit1504 <kaurkamaljit963@gmail.com>
2019-11-05 17:31:39 -0500

    cc

--- codewithChris/ViewController.swift
+++ codewithChris/ViewController.swift
@@ -10,10 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var lblusername: UILabel!
+    @IBOutlet weak var lblpassword: UILabel!
+    var uDefault : UserDefaults!
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var btnReMMe: UISwitch!
+    
+override func viewDidLoad() {
+        super.viewDidLoad()        // Do any additional setup after loading the view.
     }
+
+    
+    @IBAction func btnlogin(_ sender: UIButton)
+    {
+    if readInformationPlist()
+    {
+      
+        if self.btnReMMe.isOn
+      {
+        self.uDefault.set(txtUsername.text, forKey: "username")
+        self.uDefault.set(txtPassword.text, forKey: "password")
+        }
+      
+      else{
+        self.uDefault.removeObject(forKey: "username")
+        self.uDefault.removeObject(forKey: "password")
+        }
+        
+      //  let sb=UIStoryboard(name: "Main", bundle: nil)
+        
+    }
+    
+    else
+    {
+        let alert = UIAlertController(title: "Wrong Username Or Password", message: "change username or password", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: {action
+            in
+            print("Cancle clicked")
+        }))
+        self.present(alert, animated: true)
+        print("invalid username or password")
+    }
+    }
+    
+    func readInformationPlist() -> Bool{
+        if let bundlePath = Bundle.main.path(forResource: "Users", ofType: "plist") {
+            let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+            let usersList = dictionary!["User"] as! NSArray
+            
+            for u in usersList
+            {
+                let user = u as! NSDictionary
+                let uname = user["username"]! as! String
+                let pwd = user["password"]! as! String
+                if uname==txtUsername.text! && pwd==txtPassword.text!
+                {
+                    return true
+                }
+            }
+            
+            
+        }
+        return false
+    }
+
 
 
 }
